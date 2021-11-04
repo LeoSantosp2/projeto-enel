214cbec12bcd4a603eaa15a3d9f9e82eb29c53de
Leonardo Santos <leogilberto2010@hotmail.com>
2021-11-04 14:58:58 -0300

    Initial Commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,10 @@
+var te = 0.25588
+var tusd = 0.25971
+
+var consumo_30_te = (72 / 100) * te
+var consumo_30_tuds = 0.65
+
 function calcular(){
-    var te = 0.25588
-    var tusd = 0.25971
     var b = window.document.getElementById('kwh')
     var c = window.document.getElementById('resultado')
     var soma = te + tusd + Number(b.value)
@@ -10,5 +14,6 @@ function calcular(){
         <li>Energia: R$${te}</li>
         <li>Uso de Sistema de Distribuição: R$${tusd}</li>
         <li>Valor Base: R$${soma.toFixed(2)}</li>
+        <li>Teste: R$${consumo_30_te.toFixed(5)}</li>
     </ul>`
 }
